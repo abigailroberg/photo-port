3bffec135b4888e1d0a958981e07728abc7a0c02
Abigail Roberg <abbyroberg@gmail.com>
2021-11-04 18:56:51 -0500

    code snapshot 20.3

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,17 +3,19 @@ import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
   const {
-      categories = [],
-      setCurrentCategory,
-      currentCategory,
-  } = props
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
 
-  const handleClick = item => {
-    return item
-  }
+
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
 
   return (
-    <header data-testid="header" className="flex-row px-1"> 
+    <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
@@ -32,20 +34,21 @@ function Nav(props) {
             </span>
           </li>
           {categories.map((category) => (
-              <li className={`mx-1 ${
+            <li
+              className={`mx-1 ${
                 currentCategory.name === category.name
                 }`}
-                key={category.name}
-              >
-                <span 
-                  onClick={() => { 
-                    setCurrentCategory(category)
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
                 }}
-                >
-                  {capitalizeFirstLetter(category.name)}
-                </span>
-              </li>
-            ))}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
